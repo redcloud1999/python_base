e0445651486d7a291ec2ecc575c16d263e5ccaf1
redcloud1999 <ccbtata3@gmail.com>
2022-08-11 19:03:11 -0300

    implementamos o hello world multi linguas

--- hello.py
+++ hello.py
@@ -1,5 +1,42 @@
 #!/usr/bin/env python3
 
+"""Hello World Multi Linguas.
 
-# comentario em python3
-print('Hello, World!')
+Dependendo da lingua configurada no ambiente
+o programa exibe a mensagem correspondente.
+
+Como usar:
+
+Tenha a variavel LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Execução:
+
+    python3 Hello.py
+    ou
+    ./hello.py
+
+"""
+
+__version__ = '0.0.1'
+__author__ = 'Otávio Trindade'
+__license__ = 'Unlicense'
+
+import os
+
+current_language =  os.getenv('LANG', 'en_US')[:5]
+
+msg = 'Hello, World!'
+
+if current_language == 'pt_BR':
+    msg = 'Olá, Mundo!'
+if current_language == 'it_IT':
+    msg = 'Ciao, Mondo!'
+if current_language == 'es_SP':
+    msg = 'Hola, Mundo!'
+if current_language == 'fr_FR':
+    msg = 'Bonjour, Monde!'
+                
+
+print(msg)
