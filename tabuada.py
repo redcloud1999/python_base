28a6197c17c988e37c10c74a7d11c2d7bee7c508
redcloud1999 <ccbtata3@gmail.com>
2022-08-16 17:14:26 -0300

    implementação de formatação de texto na tabuada

--- tabuada.py
+++ tabuada.py
@@ -1,35 +1,38 @@
 #!/usr/bin/env python
 """Imprime a tabuada do 1 ao 10
 
-Tabuada do 1
-1
-2
-3
+---Tabuada do 1---
+
+    1 X 1 = 2
+    2 X 1 = 2
+    3 X 1 = 3
 
 ...
-_______________________
+###################
+---Tabuada do 2---
 
-Tabuada do 2
-2
-4
+    2 X 1 = 2
+    2 X 2 = 4
 
 ...
+###################
 
-_______________________
 
 """
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = 'Otávio'
 
-# base = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+base = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 numeros =  list(range(1, 11))
 
-for numero in numeros:
-    print('Tabuada do:', numero)
-    for outro_numero in numeros:
-        print(numero * outro_numero)
-        
-    print('_______________________')
+for n1 in numeros:
+    print('{:-^20}'.format(f'Tabuada do {n1}'))
+    print('\n')
+    for n2 in numeros:
+        resultado = n1 * n2
+        print('{:^20}'.format(f'{n1} X {n2} = {resultado}'))
+    print(20 * '#')
+  
 
 
 
