882fa6811c58c95fc1dc540a05f74da20b87707d
redcloud1999 <ccbtata3@gmail.com>
2022-08-22 16:24:12 -0300

    implementação de dicionarios no script de cadastro de produtos

--- cadastro_de_produto.py
+++ cadastro_de_produto.py
@@ -3,13 +3,35 @@
 '''Cadastro de produto'''
 __version__ = '0.1.0'
 
+from pprint import pprint
+produto = {
+    'nome': 'Caneta',
+    'cores': ['azul', 'branco'], 
+    'preco': 3.23,
+    'dimensao': {
+        'altura': 12.1,
+        'largura': 0.8, 
+    },
+    'em_estoque': True,
+    'codigo': 45678,
+    'codebar': None, 
+}
 
+cliente = {
+    'nome': 'Otavio'
+}
 
+compra = {
+    'cliente': cliente,
+    'produto': produto,
+    'quantidade': 3,
+}
 
+total_compra = compra['quantidade'] * compra['produto']['preco']
 
-
-
-
+pprint(f'O cliente {compra["cliente"]["nome"]}'
+       f' comprou {compra["quantidade"]} unidades de {compra["produto"]["nome"]}'
+       f' e pagou o total de {total_compra}')
 
 
 
