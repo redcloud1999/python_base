059d422cb364010b642c1b0c8f027b47b21fe8b1
redcloud1999 <ccbtata3@gmail.com>
2022-09-23 18:10:25 -0300

    implementação de envio de emails

--- interpolação.py
+++ interpolação.py
@@ -1,5 +1,10 @@
+#!/usr/bin/env python3
+
 import os
 import sys
+import smtplib
+from email.mime.text import MIMEText
+
 
 arguments = sys.argv[1:]
 if not arguments:
@@ -13,21 +18,31 @@ path = os.curdir
 filepath = os.path.join(path, filename)
 templatepath = os.path.join(path, templatename)
 
+with smtplib.SMTP(host='localhost', port=8025) as server:
 
-for line in open(filepath):
-    name, email = line.split(',')
 
-    # TODO: substituir por envio de email
-    print(f'Enviando email para {email}')
-    print(
-        open(templatepath).read() 
-        % {
-            'nome': name, 
-            'produto': 'Caneta',
-            'texto': 'Escreve muito bem',
-            'link': 'https://canetaslegais.com',
-            'quantidade': 1,
-            'preco': 50.5
-        }
+
+    for line in open(filepath):
+        name, email = line.split(',')
+        text = (
+            open(templatepath).read() 
+            % {
+                'nome': name, 
+                'produto': 'Caneta',
+                'texto': 'Escreve muito bem',
+                'link': 'https://canetaslegais.com',
+                'quantidade': 1,
+                'preco': 50.5
+            }
         )
-    print("-" * 50)
+
+        from_ = 'trindade_otavio@hotmail.com'
+        to = ', '.join([email])
+        message = MIMEText(text, 'html')
+        message['Subject'] = 'Compre mais'
+        message['from'] = from_
+        message['To'] = to
+
+        server.sendmail(from_, to, message.as_string())
+
+        
